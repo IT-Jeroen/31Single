c827d88d0970e398c726cafc2d6ba073a50fc064
IT-Jeroen <55962983+IT-Jeroen@users.noreply.github.com>
2023-11-30 13:07:54 +1100

    Delete src/js directory

--- src/js/players.js
+++
@@ -1,7 +0,0 @@
-export const players = {
-    0: {"name":'Local Player', "location": 'south', 'cards-in-hand':{}, 'wins': 0, 'orientation': 'matrix0', 'pass': false, 'active':false},
-    1: {"name":'Ziva', "location": 'west', 'cards-in-hand':{}, 'wins': 0, 'orientation': 'matrix90', 'pass': false, 'active':false},
-    2: {"name":'Dad', "location": 'north', 'cards-in-hand':{}, 'wins': 0, 'orientation': 'matrix180', 'pass': false, 'active':false},
-    3: {"name":'Mum', "location": 'east', 'cards-in-hand':{}, 'wins': 0, 'orientation': 'matrix270', 'pass': false, 'active':false},
-    4: {"name":'Bank', "location": 'center', 'cards-in-hand':{}, 'wins': 0, 'orientation': 'matrix0', 'pass': true, 'active':false},
-}
